d27a55b05f1ac0643a019144d8464f029bd9be80
Naman Gupta <namang94@gmail.com>
2015-12-21 12:18:06 +0530

    Ex2 edit

---
+++ ex2/plotDecisionBoundary_non_linear.m
@@ -0,0 +1,17 @@
+function plotDecisionBoundary_non_linear(theta, X,y)
+    u = linspace(-1, 1.5, 50);
+    v = linspace(-1, 1.5, 50);
+
+    z = zeros(length(u), length(v));
+    % Evaluate z = theta*x over the grid
+    for i = 1:length(u)
+        for j = 1:length(v)
+            z(i,j) = mapFeature(u(i), v(j))*theta;
+        end
+    end
+    z = z'; % important to transpose z before calling contour
+    % Plot z = 0
+    % Notice you need to specify the range [0, 0]
+    contour(u, v, z, [0, 0], 'LineWidth', 2)
+    
+end
